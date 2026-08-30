9e82d30e73e9d89d5acfd1c9c62f3b759b0fada4
creativeloc <creativeloc@yahoo.com>
2026-08-30 10:52:06 -0400

    implememted latest collections header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/home/HeroSection"
+import LatestCollections from "@/components/home/LatestCollections"
 import FrontendLayout from "@/components/layouts/FrontendLayout"
 
 export default function Home() {
   return (
     <FrontendLayout>
       <HeroSection />
+      <LatestCollections />
     </FrontendLayout>
   )
 }
