991fc10691c260865cd2c61c2bf5600eb10f6e29
creativeloc <creativeloc@yahoo.com>
2026-09-01 05:07:13 -0400

    started product page comp onent

---
+++ src/app/products/[productid]/page.tsx
@@ -0,0 +1,10 @@
+import FrontendLayout from "@/components/layouts/FrontendLayout"
+import ProductPageComponent from "../ProductPageComponent"
+
+export default function page() {
+  return (
+    <FrontendLayout>
+      <ProductPageComponent />
+    </FrontendLayout>
+  )
+}
