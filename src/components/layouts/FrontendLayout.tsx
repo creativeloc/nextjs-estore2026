8240e0a83fc7de182e15398ff09561715527e561
Lawrence Caulker <locaulker@gmail.com>
2026-09-06 17:47:57 -0400

    installed zustand for global functionality. Added search bar ui

--- src/components/layouts/FrontendLayout.tsx
+++ src/components/layouts/FrontendLayout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../navbar/Navbar"
 import Footer from "../home/Footer"
+import SearchInput from "../ui/SearchInput"
 
 export default function FrontendLayout({
   children
@@ -9,6 +10,7 @@ export default function FrontendLayout({
   return (
     <>
       <Navbar />
+      <SearchInput />
       <div className="mx-auto max-w-7xl px-4 w-full sm:px-6 lg:px-8">
         {children}
       </div>
